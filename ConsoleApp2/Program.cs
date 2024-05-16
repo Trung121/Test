49ae6f7f0c29efc5d14018bd1ff6e52ad20ac2c7
LAPTOP-GJS5U5P6\hp <nguyenbabaotrung@gmail.com>
2024-05-16 08:58:20 +0700

    fist commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+	public class Test1
+	{
+		public string Name { get { return name; }
+			set { name = value; } }
+
+		public int Age {
+			get { return age; }
+			set { age = value; }
+		}
+
+		private string name = "Test";
+
+		private int age = 10;
+	}
+
+	public class Test2 : Test1
+	{
+		
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Test1 test = new Test1();
+			Console.WriteLine("{0} : {1}", test.Name, test.Age);
+			test.Name = "Trung";
+			test.Age = 25;
+			Console.WriteLine("{0} : {1}", test.Name, test.Age );
+			Console.ReadLine();
+		}
+	}
+}
